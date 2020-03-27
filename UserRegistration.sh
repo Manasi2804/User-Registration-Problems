e8440717e1de43b5432bf3548484f8628895b80b
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-27 20:40:53 +0530

    Added:To validate last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -11,3 +11,13 @@ then
 else
 	echo Invalid
 fi
+
+read -p "Enter the last name:" lastName
+patternForLastName="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $lastName =~ $patternForLastName ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
