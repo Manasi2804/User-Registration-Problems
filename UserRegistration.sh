d62c5105286c69be799f1b60a4d9deb370f288a4
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-27 21:32:55 +0530

    Added: To validate mobile number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,31 +1,32 @@
 #!/bin/bash -x
 
-echo "Welcome to  user registration problems"
+COUNTRY_CODE=91
 
-#FirstName
-read -p "Enter the first name:" firstName
-patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
-
-if [[ $firstName =~ $patternForFirstName ]]
-then
-	echo Valid
-else
-	echo Invalid
-fi
-
-#LastName
-read -p "Enter the last name:" lastName
-patternForLastName="^[A-Z]{1}[a-z]{2,}$"
-
-if [[ $lastName =~ $patternForLastName ]]
-then
-	echo Valid
-else
-	echo Invalid
-fi
-
-#Email-Id
-read -p "Enter your email id:" emailId
+function validateFirstName()
+{
+	read -p "Enter the first name:" firstName
+	patternForFirstName="^[A-Z]{1}[a-z]{2,}$"
+	if [[ $firstName =~ $patternForFirstName ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+function validateLastName()
+{
+	read -p "Enter the last name:" lastName
+	patternForLastName="^[A-Z]{1}[a-z]{2,}$"
+	if [[ $lastName =~ $patternForLastName ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+function validateEmailId()
+{
+	read -p "Enter your email id:" emailId
 	patternForEmailId="^[a-zA-Z]+([.]?[+\-a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-z]{2,4}([.]?[a-z]{2,4})?$"
 	if [[ $emailId =~ $patternForEmailId ]]
 	then
@@ -33,3 +34,20 @@ read -p "Enter your email id:" emailId
 	else
 		echo Invalid
 	fi
+}
+function validateMobileNumber()
+{
+	read -p "Enter your mobile number:" mobileNumber
+	patternForMobileNumber="^$COUNTRY_CODE[ ][9876]{1}[0-9]{9}"
+	if [[ $mobileNumber =~ $patternForMobileNumber ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
+echo "Welcome to user registration problem "
+validateFirstName
+validateLastName
+validateEmailId
+validateMobileNumber
