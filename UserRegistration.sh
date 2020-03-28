ceade6f5408b4af1f18f5f6a3cb89a73ab56fe91
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 07:35:47 +0530

    Added:To validate password according to rule 1

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -46,8 +46,21 @@ function validateMobileNumber()
 		echo Invalid
 	fi
 }
+
+function validatePassword()
+{
+	read -p "Enter your password:" password
+	patternForPassword="^.{8}$"
+	if [[ $password =~ $patternForPassword ]]
+	then
+		echo Valid
+	else
+		echo Invalid
+	fi
+}
 echo "Welcome to user registration problem "
 validateFirstName
 validateLastName
 validateEmailId
 validateMobileNumber
+validatePassword
